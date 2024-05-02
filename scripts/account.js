8a5433eaff7ea345ed38bfe4a30117a57b82cafe
Oleg <tsukuyomi1dream@icloud.com>
2024-05-02 17:59:38 +0000

    create login form validaition

--- scripts/account.js
+++ scripts/account.js
@@ -1,2 +1,16 @@
-const errorContainer = document.querySelector('.error-message');
-errorContainer.textContent = 'aboba'
+const loginForm = document.querySelector('form');
+const loginField = loginForm.querySelector('#login-field');
+const passwordField = loginForm.querySelector('#password-field');
+const submitButton = loginForm.querySelector('.login-button');
+const errorContainer = loginForm.querySelector('.error-message');
+
+submitButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if (loginField.value.length < 1) {
+        errorContainer.textContent = 'Пожалуйста, введите логин';
+    } else if (passwordField.value.length < 1) {
+        errorContainer.textContent = 'Пожалуйста, введите пароль';
+    } else {
+        errorContainer.textContent = 'Неправильный логин или пароль';
+    }
+});
