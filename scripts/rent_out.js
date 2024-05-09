a255c5e10ec1a4335f42f8d8575df4729aa4c9e5
Oleg <tsukuyomi1dream@icloud.com>
2024-05-09 17:12:07 +0000

    created modal window

--- scripts/rent_out.js
+++ scripts/rent_out.js
@@ -1,11 +1,24 @@
 const phoneField = document.querySelector('#phone-field');
 const errorContainer = document.querySelector('.error-message');
 const submitButton = document.querySelector('.button');
+const modalWindow = document.querySelector('#rent_out_success')
+const modalButton = document.querySelector('.modal-button')
 
 submitButton.addEventListener('click', (evt) => {
     evt.preventDefault();
-    if (phoneField.value.length < 1) 
+    if (phoneField.value.length < 1) {
         errorContainer.textContent = 'Это поле не должно быть пустым';
-    else 
+        return;
+    } else {
         errorContainer.textContent = '';
-})
+        modalWindow.classList.add('open');
+    }
+});
+
+modalButton.addEventListener('click', (evt) => {
+    modalWindow.classList.remove('open');
+});
+
+modalWindow.addEventListener('click', (evt) => {
+    modalWindow.classList.remove('open');
+});
