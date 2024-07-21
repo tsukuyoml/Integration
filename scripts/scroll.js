34317243b0ae5beff98348cce01158c470d56a70
Oleg <tsukuyomi1dream@icloud.com>
2024-07-21 20:12:10 +0000

    create scroll for reviews and adaptive for connection

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -15,3 +15,29 @@ function handleScroll(event, targetClass='') {
     target.scrollLeft += event.deltaY;
     console.log(target.scrollLeft, event.deltaY);
 };
+
+function moveLeft(event, targetClass) {
+    event.preventDefault();
+
+    let target = document.querySelector(targetClass);
+
+    if (isNaN(target.scrollLeft)) {
+        target.scrollLeft = 0;
+    };
+
+    target.scrollLeft += -100;
+    console.log(target.scrollLeft);
+}
+
+function moveRight(event, targetClass) {
+    event.preventDefault();
+
+    let target = document.querySelector(targetClass);
+
+    if (isNaN(target.scrollLeft)) {
+        target.scrollLeft = 0;
+    };
+
+    target.scrollLeft += 100;
+    console.log(target.scrollLeft);
+}
